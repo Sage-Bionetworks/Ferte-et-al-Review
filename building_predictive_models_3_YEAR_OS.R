cd01120264143551fb9eb1ea6dc2b56acba4c17c
Charles Ferte <cferte@belltown.fhcrc.org>
2012-10-11 11:37:35 -0700

    adressing the different step of the predictive modeling in this study

--- building_predictive_models_3_YEAR_OS.R
+++ building_predictive_models_3_YEAR_OS.R
@@ -5,3 +5,20 @@
 
 # code to develop the model in the Director's dataset (TS) and validate it in the Zhu dataset (VS)
 # we want to predict the 3 year propbability of OS
+
+# load the packages that are neeeded
+
+# load the data from synapse
+
+# first build the model based on clin variables only (linear regression)
+
+# second build the model based on linear regression of clin + ge features
+
+# thrird build the model based on clin + molecular features using elasticnet
+
+# thrird build the model based on clin + molecular features using RandomForest
+
+# adress the results of in the VS using ROC curves
+
+# adress the same results of AUC in boxplots with their IC95
+
