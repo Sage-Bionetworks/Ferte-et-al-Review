8cf45e3797bb78334f77ec81c3a2d7fb24ae72b5
Charles Ferte <cferte@belltown.fhcrc.org>
2012-10-18 16:14:21 -0700

    polish the first part (svd plots showiing the data transformation)

--- building_predictive_models_3_YEAR_OS.R
+++ building_predictive_models_3_YEAR_OS.R
@@ -1,6 +1,6 @@
-# Charles Ferté
+# Charles Ferté,MD,MSc
 # 10/11/2012
-#Sage Bionetworks
+# Sage Bionetworks
 
 # code to develop the model in the Director's dataset (TS) and validate it in the Zhu dataset (VS)
 # we want to predict the 3 year propbability of OS
@@ -34,12 +34,17 @@ rm(tmp)
 # transform zhu eset in matrix
 zhu <- exprs(zhu)
 
+#check if there is any latent structure in the data (are the datasets comparable ?)
+s <- svd(cbind(dir,zhu))
 par(mfrow=c(2,2))
+plot(s$v[,1],s$v[,2],col=c(rep("black",times=299),rep("red",times=62)),pch=20,xlab="PC1",ylab="PC2",main="initial datasets")
+
+# quantile normalize zhu to have the same disribution than dir (make them comparable)
+zhu <- normalize2Reference(zhu,rowMeans(dir))
 
 #check if there is any latent structure in the data (are the datasets comparable ?)
 s <- svd(cbind(dir,zhu))
-plot(s$v[,1],s$v[,2],col=c(rep("black",times=299),rep("red",times=62)),pch=20,xlab="PC1",ylab="PC2",main="PCA on the dir & zhu datasetsgrouped together
-black and red labels stand for the dataset of origin")
+plot(s$v[,1],s$v[,2],col=c(rep("black",times=299),rep("red",times=62)),pch=20,xlab="PC1",ylab="PC2",main="after quantile normalization")
 
 # get rid of the junk features
 zhu <- zhu[-c(grep("AFFX",rownames(zhu))),]
@@ -47,8 +52,7 @@ dir <- dir[rownames(zhu),]
 
 #check if there is any latent structure in the data (are the datasets comparable ?)
 s <- svd(cbind(dir,zhu))
-plot(s$v[,1],s$v[,2],col=c(rep("black",times=299),rep("red",times=62)),pch=20,xlab="PC1",ylab="PC2",main="PCA on the dir & zhu datasetsgrouped together
-black and red labels stand for the dataset of origin")
+plot(s$v[,1],s$v[,2],col=c(rep("black",times=299),rep("red",times=62)),pch=20,xlab="PC1",ylab="PC2",main="")
 
 # focus on the most variant probes
 prob.var1  <- apply(zhu,1,var)
@@ -63,8 +67,7 @@ s <- svd(cbind(dir,zhu))
 plot(s$v[,1],s$v[,2],col=c(rep("black",times=299),rep("red",times=62)),pch=20,xlab="PC1",ylab="PC2",main="PCA on the dir & zhu datasetsgrouped together
 black and red labels stand for the dataset of origin")
 
-# # quantile normalize zhu to have the same disribution than dir (make them comparable)
-# zhu <- normalize2Reference(zhu,rowMeans(dir))
+
 # 
 # #check if there is any latent structure in the data (are the datasets comparable ?)
 # s <- svd(cbind(dir,zhu))
@@ -106,9 +109,11 @@ rownames(z)[(length(rownames(z))-2):length(rownames(z))] <- c("P_Stage","GENDER"
 ######################################################################################################################################
 fit <- glm(dir_clin$y_dir~dir_clin$P_Stage + dir_clin$GENDER + dir_clin$Age,data=as.data.frame(t(dir_clin)),family="binomial")
 
-# predict in zhu
-#yhat <- predict(fit,newdata=as.data.frame(t(zhu_clin)),type="response",na.action = na.omit)
-#yhat
+#predict in zhu
+yhat <- predict(fit,newdata=as.data.frame(t(zhu_clin)),type="response",na.action = na.omit)
+yhat
+
+## there is a problem here -> yhat should have only 62 coeff !!! and not 299 !
 
 par(mfrow=c(1,1))
 boxplot(fit$fitted.values~dir_clin$y_dir,ylab="3-year OS prediction (%)",xlab="3-year OS",main="logit model - clinical variables only")
