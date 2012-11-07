cd814efb3554d6ee2903ef7bcb8d075c9f624547
Charles Ferte <cferte@belltown.fhcrc.org>
2012-11-07 15:52:42 -0800

    hope this is finished to be polished now !!!

--- dataProcessing/Norm.R
+++ dataProcessing/Norm.R
@@ -111,74 +111,78 @@ dim(myNormSummarized)
 tmp <- paste(dataset,"_snm",sep="") #put SNM at the end of the names
 assign(tmp,myNormSummarized)
 
-###################################################################################
+##################################################################################
 # save the data in Synapse
-#
+# 
 # Saving steps for initial entities back to Synapse.  
 # The ids will be accessible in the remainder of the code.
 # all of this code is commented out to ensure that there is no change to the entities
 # that already exist.
-###################################################################################
-#require(synapseClient)
-#synapseLogin()
-#
-#lusc_rma <- Data(list(name = "dir rma", parentId = 'syn87682'))
-#lusc_rma <- createEntity(lusc_rma)
+##################################################################################
+require(synapseClient)
+synapseLogin()
+
+lusc_rma <- Data(list(name = "lusc_rma", parentId = 'syn87682'))
+lusc_rma <- createEntity(lusc_rma)
 
 # add object into the data entity
-#lusc_rma <- addObject(lusc_rma,lusc_rma)
+lusc_rma <- addObject(lusc_rma,Lusc_rma)
 
 # push the raw data into this entity
-#lusc_rma <- storeEntity(entity=lusc_rma)
+lusc_rma <- storeEntity(entity=lusc_rma)
 
-#############################################################################################
-#lusc_gcrma <- Data(list(name = "dir gcrma", parentId = 'syn87682'))
-#lusc_gcrma <- createEntity(lusc_gcrma)
+############################################################################################
+
+lusc_gcrma <- Data(list(name = "lusc_gcrma", parentId = 'syn87682'))
+lusc_gcrma <- createEntity(lusc_gcrma)
 
 # add object into the data entity
-#lusc_gcrma <- addObject(lusc_gcrma,lusc_gcrma)
+lusc_gcrma <- addObject(lusc_gcrma,Lusc_gcrma)
 
 # push the raw data into this entity
-#lusc_gcrma <- storeEntity(entity=lusc_gcrma)
-#############################################################################################
+lusc_gcrma <- storeEntity(entity=lusc_gcrma)
 
+############################################################################################
 
-#lusc_MAS5 <- Data(list(name = "dir MAS5", parentId = 'syn87682'))
-#lusc_MAS5 <- createEntity(lusc_MAS5)
+lusc_MAS5 <- Data(list(name = "lusc_MAS5", parentId = 'syn87682'))
+lusc_MAS5 <- createEntity(lusc_MAS5)
+
+#add object into the data entity
+lusc_MAS5 <- addObject(lusc_MAS5,Lusc_MAS5)
+
+#push the raw data into this entity
+lusc_MAS5 <- storeEntity(entity=lusc_MAS5)
+
+############################################################################################
+
+lusc_dCHIP <- Data(list(name = "lusc_dCHIP", parentId = 'syn87682'))
+lusc_dCHIP <- createEntity(lusc_dCHIP)
 
 # add object into the data entity
-#lusc_MAS5 <- addObject(lusc_MAS5,lusc_MAS5)
+lusc_dCHIP <- addObject(lusc_dCHIP,Lusc_dCHIP)
 
 # push the raw data into this entity
-#lusc_MAS5 <- storeEntity(entity=lusc_MAS5)
-#############################################################################################
-#lusc_dCHIP <- Data(list(name = "dir dCHIP", parentId = 'syn87682'))
-#lusc_dCHIP <- createEntity(lusc_dCHIP)
+lusc_dCHIP <- storeEntity(entity=lusc_dCHIP)
+
+############################################################################################
+
+lusc_frma <- Data(list(name = "lusc_frma", parentId = 'syn87682'))
+lusc_frma <- createEntity(lusc_frma)
+
+#add object into the data entity
+lusc_frma <- addObject(lusc_frma,Lusc_frma)
+
+#push the raw data into this entity
+lusc_frma <- storeEntity(entity=lusc_frma)
+
+############################################################################################
+lusc_snm <- Data(list(name = "lusc_snm", parentId = 'syn87682'))
+lusc_snm <- createEntity(lusc_snm)
 
 # add object into the data entity
-#lusc_dCHIP <- addObject(lusc_dCHIP,lusc_dCHIP)
+lusc_snm <- addObject(lusc_snm,Lusc_snm)
 
 # push the raw data into this entity
-#lusc_dCHIP <- storeEntity(entity=lusc_dCHIP)
-#############################################################################################
-
-#lusc_frma <- Data(list(name = "dir frma", parentId = 'syn87682'))
-#lusc_frma <- createEntity(lusc_frma)
-
-# add object into the data entity
-#lusc_frma <- addObject(lusc_frma,lusc_frma)
-
-# push the raw data into this entity
-#lusc_frma <- storeEntity(entity=lusc_frma)
-
-#############################################################################################
-# lusc_snm <- Data(list(name = "lusc_snm", parentId = 'syn87682'))
-# lusc_snm <- createEntity(lusc_snm)
-# 
-# # add object into the data entity
-# lusc_snm <- addObject(lusc_snm,Lusc_snm)
-# 
-# # push the raw data into this entity
-# lusc_snm <- storeEntity(entity=lusc_snm)
+lusc_snm <- storeEntity(entity=lusc_snm)
 
 
